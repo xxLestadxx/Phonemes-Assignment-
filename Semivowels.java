bf22995dd42f1ecd74955996b755da264b88ef97
Yordan Gospodinov <j.v.gospodinov@gmail.com>
2017-08-12 09:59:38 +0300

    Update Semivowels.java

--- Semivowels.java
+++ Semivowels.java
@@ -1,8 +1,4 @@
-/* 
- * Student number : 2422647
- * CSCU9T4
- * version: March2017
- */
+
 public class Semivowels extends Phonemes {
 
 	public Semivowels(double start, double end, String phoneme) {
